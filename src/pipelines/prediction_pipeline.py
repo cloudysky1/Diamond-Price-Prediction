99f4f44648c8ebeed8ed0e9217fe11021fb7a9f5
Vansh Singla <singlavansh435@gmail.com>
2025-09-15 13:20:20 +0530

    Update

--- src/pipelines/prediction_pipeline.py
+++ src/pipelines/prediction_pipeline.py
@@ -12,8 +12,8 @@ class PredictPipeline:
     
     def predict(self, features):
         try:
-            preprocessor_path = os.path.join("Artifacts", "preprocessor.pkl")
-            model_path = os.path.join("Artifacts", "model.pkl")
+            preprocessor_path = os.path.join("C:\MLProjects\DiamondPricePrediction2\artifacts\preprocessor.pkl")
+            model_path = os.path.join("C:\MLProjects\DiamondPricePrediction2\artifacts\model.pkl")
             
 
             preprocessor = load_object(preprocessor_path)
