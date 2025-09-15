e1574c9f2c43f7eed5b4682a051348ef6818bba9
Vansh Singla <singlavansh435@gmail.com>
2025-09-15 10:51:38 +0530

    Initial commit with project files

--- src/pipelines/prediction_pipeline.py
+++ src/pipelines/prediction_pipeline.py
@@ -1,75 +1,71 @@
 import os
 import sys
 import pandas as pd
-from src.exception import customexception
+from src.exception import CustomException
 from src.logger import logging
-from src.utils.utils import load_object
+from src.utils import load_object
 
 
 class PredictPipeline:
     def __init__(self):
         pass
     
-    def predict(self,features):
+    def predict(self, features):
         try:
-            preprocessor_path=os.path.join("Artifacts","preprocessor.pkl")
-            model_path=os.path.join("Artifacts","model.pkl")
+            preprocessor_path = os.path.join("Artifacts", "preprocessor.pkl")
+            model_path = os.path.join("Artifacts", "model.pkl")
             
-            preprocessor=load_object(preprocessor_path)
-            model=load_object(model_path)
+            preprocessor = load_object(preprocessor_path)
+            model = load_object(model_path)
             
-            scaled_data=preprocessor.transform(features)
-            
-            pred=model.predict(scaled_data)
+            scaled_data = preprocessor.transform(features)
+            pred = model.predict(scaled_data)
             
             return pred
-            
-            
         
         except Exception as e:
-            raise customexception(e,sys)
+            # Use correct class name
+            raise CustomException(e, sys)
     
-    
-    
+
 class CustomData:
     def __init__(self,
-                 carat:float,
-                 depth:float,
-                 table:float,
-                 x:float,
-                 y:float,
-                 z:float,
-                 cut:str,
-                 color:str,
-                 clarity:str):
+                 carat: float,
+                 depth: float,
+                 table: float,
+                 x: float,
+                 y: float,
+                 z: float,
+                 cut: str,
+                 color: str,
+                 clarity: str):
         
-        self.carat=carat
-        self.depth=depth
-        self.table=table
-        self.x=x
-        self.y=y
-        self.z=z
+        self.carat = carat
+        self.depth = depth
+        self.table = table
+        self.x = x
+        self.y = y
+        self.z = z
         self.cut = cut
         self.color = color
         self.clarity = clarity
             
-                
     def get_data_as_dataframe(self):
-            try:
-                custom_data_input_dict = {
-                    'carat':[self.carat],
-                    'depth':[self.depth],
-                    'table':[self.table],
-                    'x':[self.x],
-                    'y':[self.y],
-                    'z':[self.z],
-                    'cut':[self.cut],
-                    'color':[self.color],
-                    'clarity':[self.clarity]
-                }
-                df = pd.DataFrame(custom_data_input_dict)
-                logging.info('Dataframe Gathered')
-                return df
-            except Exception as e:
-                logging.info('Exception Occured in prediction pipeline')
-                raise customexception(e,sys)
+        try:
+            custom_data_input_dict = {
+                'carat': [self.carat],
+                'depth': [self.depth],
+                'table': [self.table],
+                'x': [self.x],
+                'y': [self.y],
+                'z': [self.z],
+                'cut': [self.cut],
+                'color': [self.color],
+                'clarity': [self.clarity]
+            }
+            df = pd.DataFrame(custom_data_input_dict)
+            logging.info('Dataframe Gathered')
+            return df
+        except Exception as e:
+            logging.info('Exception Occurred in prediction pipeline')
+            raise CustomException(e, sys)
--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -1,23 +1,21 @@
-import os
-import sys
-from src.logger import logging
-from src.exception import CustomException
-import pandas as pd
+from src.DiamondPricePrediction.components.Data_ingestion import DataIngestion
 
-from src.components.data_ingestion import DataIngestion
+from src.DiamondPricePrediction.components.Data_transformation import DataTransformation
 
-from src.components.data_transformation import DataTransformation
-from src.components.model_trainer import ModelTrainer
+from src.DiamondPricePrediction.components.Model_trainer import ModelTrainer
+
+from src.DiamondPricePrediction.components.Model_evaluation import ModelEvaluation
 
 
-if __name__=='__main__':
-    obj=DataIngestion()
-    train_data_path,test_data_path=obj.initiate_data_ingestion()
-    print(train_data_path,test_data_path)
 
-    data_transformation=DataTransformation()
+obj=DataIngestion()
+train_data_path,test_data_path=obj.initiate_data_ingestion()
 
-    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+data_transformation=DataTransformation()
+train_arr,test_arr=data_transformation.initialize_data_transformation(train_data_path,test_data_path)
 
-    model_trainer=ModelTrainer()
-    model_trainer.initate_model_training(train_arr,test_arr)
+model_trainer_obj=ModelTrainer()
+model_trainer_obj.initate_model_training(train_arr,test_arr)
+
+model_eval_obj = ModelEvaluation()
+model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
